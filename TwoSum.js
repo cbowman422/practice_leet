c48c9b9e60c1422d2372f5c165b7492c33bfe8e1
cbowman422 <chrisb0wman@hotmail.com>
2023-07-27 16:29:34 -0400

    hash method 1 with console logs

--- TwoSum.js
+++ TwoSum.js
@@ -7,10 +7,12 @@ var twoSum = function(nums, target) {
 
   for(let i = 0; i < nums.length; i++){
    indices[nums[i]] = i;
+   console.log(indices)
   }
 
   for(let i = 0; i < nums.length; i++){
     const complement = target - nums[i];
+    console.log(indices[complement])
 
     if( indices[complement] !== undefined && i !== indices[complement]){
       return [i, indices[complement]]
@@ -18,4 +20,4 @@ var twoSum = function(nums, target) {
    }
 };
 
-console.log(twoSum([1,2,3,4,5],6));
+console.log(twoSum([9,3,3,4,5],6));
