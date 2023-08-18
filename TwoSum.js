8d40ae4d2aab7c3b008d1281ba322e65e5944516
cbowman422 <chrisb0wman@hotmail.com>
2023-08-18 10:17:03 -0400

    two sum

--- TwoSum.js
+++ TwoSum.js
@@ -21,3 +21,16 @@ var twoSum = function(nums, target) {
 };
 
 console.log(twoSum([9,3,3,4,5],6));
+
+
+var twoSum = function(nums, target) {
+  let arry = []
+  for(let i=0; i < nums.length; i++){
+      for(let j=0; j < nums.length; j++){
+          if (nums[i] + nums[j] === target && arry.length < 2 && i !== j){
+              arry.push(i,j)
+          } 
+      }
+  }
+  return arry
+};
